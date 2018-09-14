cf994b1ccbfd7b10f87a5b9323fd53a76c24db3f
Perry A Branch <perry.branch@galvanize.com>
2018-09-14 15:40:46 -0400

    solved

--- src/Playlist.js
+++ src/Playlist.js
@@ -1,10 +1,36 @@
 class Playlist{
-  constructor(){
-
+  constructor(name){
+    this.name = name
+    this.songs = []
   }
 
   isEmpty(){
+    return this.songs.length === 0
+  }
 
+  addSong(song){
+    this.songs.push(song)
+  }
+
+  songNames(){
+    return this.songs.map((song) => song.name)
+  }
+
+  totalDuration(){
+    return this.songs.reduce((acc, song) => acc += song.duration, 0)
+  }
+
+  swap(song1, song2){
+    const index1 = this.songs.indexOf(song1)
+    const index2 = this.songs.indexOf(song2)
+
+    const temporarySong = this.songs[index1]
+    this.songs[index1] = this.songs[index2]
+    this.songs[index2] = temporarySong
+  }
+
+  removeSong(song){
+    this.songs = this.songs.filter((s) => song.name != s.name)
   }
 }
 
